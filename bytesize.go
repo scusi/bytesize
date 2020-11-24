e0fefa0218371ac41b5c55d9092fa919de1be7fd
Florian Walther <flw@scusi.io>
2020-11-24 23:47:31 +0100

    corrected example code

--- bytesize.go
+++ bytesize.go
@@ -15,7 +15,7 @@ package bytesize
 // defer file.Close()
 // fileInfo, err := file.Stat()
 // check(err)
-// fileSize := ByteSize.ByteSize(int64(fileInfo.Size()))
+// fileSize := bytesize.ByteSize(int64(fileInfo.Size()))
 // fmt.Printf("Filesize is: %s\n", fileSize)
 //
 
@@ -51,21 +51,21 @@ const (
 func (b ByteSize) String() string {
 	switch {
 	case b >= YB:
-		return fmt.Sprintf("%.2fYB", b/YB)
+		return fmt.Sprintf("%.2f YB", b/YB)
 	case b >= ZB:
-		return fmt.Sprintf("%.2fZB", b/ZB)
+		return fmt.Sprintf("%.2f ZB", b/ZB)
 	case b >= EB:
-		return fmt.Sprintf("%.2fEB", b/EB)
+		return fmt.Sprintf("%.2f EB", b/EB)
 	case b >= PB:
-		return fmt.Sprintf("%.2fPB", b/PB)
+		return fmt.Sprintf("%.2f PB", b/PB)
 	case b >= TB:
-		return fmt.Sprintf("%.2fTB", b/TB)
+		return fmt.Sprintf("%.2f TB", b/TB)
 	case b >= GB:
-		return fmt.Sprintf("%.2fGB", b/GB)
+		return fmt.Sprintf("%.2f GB", b/GB)
 	case b >= MB:
-		return fmt.Sprintf("%.2fMB", b/MB)
+		return fmt.Sprintf("%.2f MB", b/MB)
 	case b >= KB:
-		return fmt.Sprintf("%.2fKB", b/KB)
+		return fmt.Sprintf("%.2f KB", b/KB)
 	}
-	return fmt.Sprintf("%.2fB", b)
+	return fmt.Sprintf("%.2f B", b)
 }
